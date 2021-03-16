650674471629ab28a4355580e09a90c27d38069d
Raul Garcia <r.garcia@ibermatica.com>
2021-03-16 18:01:37 +0000

    BORRADO TARGET

--- src/main/java/com/curso/Libreria.java
+++ src/main/java/com/curso/Libreria.java
@@ -33,6 +33,11 @@ public class Libreria
     {
          return numero1 - numero2 ;
     }
+    
+            public static int multiplicar (int numero1 , int numero2 ) 
+    {
+         return numero1 * numero2 ;
+    }
      
     /*         public static void  main (String[]arg ) 
     {
