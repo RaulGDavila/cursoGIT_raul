5bdbaaf95295dde4cfd0c7584b5ac76c26470a06
Raul Garcia <r.garcia@ibermatica.com>
2021-03-16 15:26:17 +0000

    Decimalesadd

---
+++ src/test/java/com/curso/LibreriaDecimalesTest.java
@@ -0,0 +1,33 @@
+
+
+package com.curso;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class LibreriaDecimalesTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( 13.8 == Libreria.suma(3.5,5.9)  );
+    }
+   
+       @Test
+    public void resta5y6()
+    {
+        assertTrue( 10.00 == Libreria.resta(5.2,6.3)  );
+    }
+      @Test
+     public void suma5y5();
+    {
+        assertTrue( 10.00 == Libreria.suma(5,5)  );
+    }
+}
