e335a3e8ee4fc2af326f07f5a63a1ea340d26c0a
Raul Garcia <r.garcia@ibermatica.com>
2021-03-16 15:25:18 +0000

    Decimales

--- src/main/java/com/curso/Libreria.java
+++ src/main/java/com/curso/Libreria.java
@@ -12,14 +12,26 @@ public class Libreria
     }
     
     
-        public static int suma (int numero1 , int numero2 ) 
+       public static int suma (int numero1 , int numero2 ) 
     {
         
-        return 0;
+        return numero1 + numero2 ;
     }
         public static int resta (int numero1 , int numero2 ) 
     {
-        return 0;
+         return numero1 - numero2 ;
+    }
+    
+    // SOBRECARGA - PARA LOS DOUBLES
+    
+        public static double suma (double numero1 , double numero2 ) 
+    {
+        
+        return numero1 + numero2 ;
+    }
+        public static double resta (double numero1 , double numero2 ) 
+    {
+         return numero1 - numero2 ;
     }
      
     /*         public static void  main (String[]arg ) 
